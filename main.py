00d4a1facae5b1fe1ea44e38b6201a86ac7cee23
Senal Perera <senalperera777@gmail.com>
2025-06-09 12:26:20 +0530

    Code update
    fixed some bugs, shortened the code, now the code is running, but user choice and computer's choice is the same

--- main.py
+++ main.py
@@ -11,30 +11,14 @@ def UserSelectOption(array):
     for item in array:
         print(f'{count}. {item}')
         count += 1
-    return input("Select item: ")
+    return array[int(input("Select item: "))-1]
 
 # Displaying and updating scores
 def ScoreManager(score, user, computer):
-    user = user.lower()
-    computer = computer.lower()
+    # Updating score and returning
+    score += scoreKeeper[user][options.index(computer)]
     print(f'Score: {score}')
-    # Setting index for user
-    if user == "rock":
-        index_1 = 0
-    elif user == "paper":
-        index_1 = 1
-    else:
-        index_1 = 2
-        
-    # Setting index for computer
-    if computer == "rock":
-        index_2 = 0
-    elif computer == "paper":
-        index_2 = 1
-    else:
-        index_2 = 2
-
-    score += scoreKeeper[index_1][index_2]
+    return score
 
 # Making the array and score
 options = ["Rock", "Paper", "Scissors"]
@@ -48,7 +32,8 @@ scoreKeeper = {"Rock" : [0, 0, 1],
 endGame = False
 while(not endGame):
     # Choices
-    computerChoice = PCSelectOption(options)
     userChoice = UserSelectOption(options)
-    ScoreManager(score, userChoice, computerChoice)
-    endGame = True if input("Do you want to continue?") == "Yes" else False
+    computerChoice = PCSelectOption(options)
+    print(f'Your choice: {userChoice}\nComputer Choice: {computerChoice}')
+    score = ScoreManager(score, userChoice, computerChoice)
+    endGame = True if input("Do you want to continue?") == "no" else False
