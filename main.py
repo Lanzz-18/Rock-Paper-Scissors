017a06f5d4c3de88d401c40772413e22e70d8c1e
Senal Perera <senalperera777@gmail.com>
2025-06-09 15:51:45 +0530

    Update 2
    Fixed the duplicating error, now fully runs with displaying scores

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+# fix the choices gettting duplicate bug
+# add error handling
+
 import random
 
 # Creating function to choose item randomly
@@ -11,18 +14,20 @@ def UserSelectOption(array):
     for item in array:
         print(f'{count}. {item}')
         count += 1
-    return array[int(input("Select item: "))-1]
+    return array[(int(input("Select item: ")))-1]
 
 # Displaying and updating scores
-def ScoreManager(score, user, computer):
+def ScoreManager(user, computer):
+    global userScore, computerScore
     # Updating score and returning
-    score += scoreKeeper[user][options.index(computer)]
-    print(f'Score: {score}')
-    return score
+    userScore += scoreKeeper[user][options.index(computer)]
+    computerScore += scoreKeeper[computer][options.index(user)]
+    print(f'Your Score: {userScore}\nComputer Score: {computerScore}')
 
 # Making the array and score
 options = ["Rock", "Paper", "Scissors"]
-score = 0
+userScore = 0
+computerScore = 0
 
 # Order of items = Rock, Paper, Scissors
 scoreKeeper = {"Rock" : [0, 0, 1],
@@ -35,5 +40,5 @@ while(not endGame):
     userChoice = UserSelectOption(options)
     computerChoice = PCSelectOption(options)
     print(f'Your choice: {userChoice}\nComputer Choice: {computerChoice}')
-    score = ScoreManager(score, userChoice, computerChoice)
+    ScoreManager(userChoice, computerChoice)
     endGame = True if input("Do you want to continue?") == "no" else False
